86040d8c6fb31397ee53946008e9c4fb7af53dfd
Wendley Zago <wendleyzago@hotmail.com>
2022-12-13 18:02:06 -0300

    Criação do dashboard

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,8 +1,11 @@
 
 import { useLocation } from "react-router-dom"
 
-import Message from "../layout/Message"
+import styles from "./Projects.module.css"
 
+import Message from "../layout/Message"
+import Container from "../layout/Container"
+import LinkButton from "../layout/LinkButton"
 
 
 const Projects = () => {
@@ -17,9 +20,15 @@ const Projects = () => {
 
 
   return (
-    <div>
-      <h1>Meus projetos</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus projetos</h1>
+        <LinkButton to="/newproject" text="Criar projeto"/>
+      </div>
       {message && <Message msg={message} type="success"/>}
+      <Container>
+        <p>Projetos...</p>
+      </Container>
     </div>
   )
 }
